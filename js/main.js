e048f78daf87478611647ff61e359084b233e659
Kenny Park <kennyparc@gmail.com>
2021-09-22 19:26:17 +0000

    Ensure that each search request is identified with a unique ID for the purpose of editing later. #2

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ var $searchBar = document.querySelector('.search-bar');
 $searchForm.addEventListener('submit', submitSearch);
 function submitSearch(event) {
   event.preventDefault();
-  var searchValue = $searchBar.value;
+  var searchValue = {};
+  searchValue.keyword = $searchBar.value;
+  searchValue.searchID = data.nextSearchId;
   data.searches.unshift(searchValue);
   data.nextSearchId++;
-  apiRequest(data.searches[0]);
+  apiRequest(data.searches[0].keyword);
 }
 
 function apiRequest(search) {
@@ -16,6 +18,15 @@ function apiRequest(search) {
   xhr.responseType = 'json';
   xhr.addEventListener('load', getMovieData);
   function getMovieData() {
+    // console.log(xhr.response.Search);
+    // data.searches[0]xhr.response.Search[0].imdbID
   }
   xhr.send();
 }
+
+// function apiRequestID(imdbID) {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'http://www.omdbapi.com/?apikey=67ac1937' + '&i=' + imdbID);
+//   xhr.responseType = 'json';
+//   // xhr.addEventListener('load', getMovieID);
+// }
