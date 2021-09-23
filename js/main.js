7941fc06895a0c2a85005aa6f0628d16c77e3831
Kenny Park <kennyparc@gmail.com>
2021-09-23 05:46:45 +0000

    Remove all console logs. #2

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 // var $form = document.querySelector('form');
 var $searchFormHome = document.querySelector('.home-form');
 var $searchFormResults = document.querySelector('.results-form');
-var $searchBar = document.querySelector('.search-bar-home');
+var $searchBarHome = document.querySelector('.search-bar-home');
 var $searchBarResults = document.querySelector('.search-bar-results');
 // var $homeView = document.querySelector('#home-content');
 var $movieJournalNav = document.querySelector('#movie-journal-nav');
 var $allView = document.querySelectorAll('.view');
+var detailedDataArray = [];
 
 $searchFormHome.addEventListener('submit', submitSearch);
 function submitSearch(event) {
   event.preventDefault();
   switchView('search-results');
   $movieJournalNav.setAttribute('class', 'movie-journal-anchor white font-roboto');
-  // $searchBarResults.value = $searchBar.value;
+  $searchBarResults.value = $searchBarHome.value;
   var searchValue = {};
-  searchValue.keyword = $searchBar.value;
+  searchValue.keyword = $searchBarHome.value;
   searchValue.searchID = data.nextSearchId;
   data.searches.unshift(searchValue);
   data.nextSearchId++;
   apiRequest(data.searches[0].keyword);
+  var $allList = document.querySelectorAll('li');
+  for (var i = 0; i < $allList.length; i++) {
+    $allList[i].remove();
+  }
 }
 
 $searchFormResults.addEventListener('submit', submitSearchResults);
 function submitSearchResults(event) {
   event.preventDefault();
-  // switchView('search-results');
-  // $movieJournalNav.setAttribute('class', 'movie-journal-anchor white font-roboto');
-  // $searchBarResults.value = $searchBar.value;
   var searchValue = {};
   searchValue.keyword = $searchBarResults.value;
   searchValue.searchID = data.nextSearchId;
@@ -46,28 +48,44 @@ function apiRequest(search) {
   xhr.responseType = 'json';
   xhr.addEventListener('load', getMovieData);
   function getMovieData() {
-    // console.log(xhr.response.Search);
-    // apiRequestID(xhr.response.imdbID);
     var ulElement = document.querySelector('ul');
     for (var i = 0; i < xhr.response.Search.length; i++) {
       var domTree = renderSearchResults(xhr.response.Search[i]);
       ulElement.appendChild(domTree);
+      apiRequestID(xhr.response.Search[i].imdbID);
     }
   }
   xhr.send();
 }
 
-// function apiRequestID(imdbID) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'http://www.omdbapi.com/?apikey=67ac1937' + '&i=' + imdbID);
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', getMovieID);
-//   function getMovieData() {
-//     console.log(xhr.response);
-//     // for (var i = 0; i < xhr.response.)
-//   }
-//   xhr.send();
-// }
+function apiRequestID(imdbID) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://www.omdbapi.com/?apikey=67ac1937' + '&i=' + imdbID);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', getMovieID);
+  function getMovieID() {
+    var requestIDData = {};
+    requestIDData.title = xhr.response.Title;
+    requestIDData.id = xhr.response.imdbID;
+    requestIDData.genre = xhr.response.Genre;
+    detailedDataArray.push(requestIDData);
+    var $pGenre = document.querySelectorAll('.search-result-genre');
+    var $renderedPosters = document.querySelectorAll('.poster-small');
+    for (var x = 0; x < $renderedPosters.length; x++) {
+      if ($renderedPosters[x].getAttribute('src') === 'N/A') {
+        $renderedPosters[x].setAttribute('src', '../images/image-unavailable.jpg');
+      }
+    }
+    for (var i = 0; i < detailedDataArray.length; i++) {
+      for (var n = 0; n < $pGenre.length; n++) {
+        if (detailedDataArray[i].id === $pGenre[n].textContent) {
+          $pGenre[n].textContent = detailedDataArray[i].genre;
+        }
+      }
+    }
+  }
+  xhr.send();
+}
 
 // DOM TREE
 /* <li class="search-results-padding">
@@ -101,7 +119,9 @@ function switchView(view) {
       $allView[i].className = 'view hidden';
     }
   }
-  $searchFormResults.value = $searchFormHome.value;
+  if (view === 'search-home') {
+    $movieJournalNav.setAttribute('class', 'movie-journal-anchor white font-roboto hidden');
+  }
 }
 
 function renderSearchResults(entry) {
@@ -131,6 +151,7 @@ function renderSearchResults(entry) {
   $pGenre.setAttribute('class', 'search-result-genre');
   $h3Title.textContent = entry.Title;
   $pYear.textContent = entry.Year;
+  $pGenre.textContent = entry.imdbID;
   $resultCard.appendChild($rowCard);
   $rowCard.appendChild($columnPoster);
   $columnPoster.appendChild($imgPoster);
@@ -143,11 +164,12 @@ function renderSearchResults(entry) {
   $rowYear.appendChild($pYear);
   $columnOneHalf.appendChild($rowGenre);
   $rowGenre.appendChild($pGenre);
+
   return $resultCard;
 }
 
-// Poster: "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-// Title: "Batman Begins"
-// Type: "movie"
-// Year: "2005"
-// imdbID: "tt0372784"
+$movieJournalNav.addEventListener('click', goHome);
+function goHome(event) {
+  switchView('search-home');
+  $searchFormHome.reset();
+}
